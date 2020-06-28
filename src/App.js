0749d6aa750b2687dbd535ff28e3bcd083bb8be0
Arsal Mehran <arsalmehran@gmail.com>
2020-06-28 05:29:43 -0700

    refactored functions that make api call to read cleaner

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Leaves />
+        {/* <Leaves /> */}
         <Space />
         <Main />
       </div>
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -16,8 +16,32 @@ class Main extends React.Component {
         });
     }
 
-    getRandomQuote = () => {
-        fetch('https://api.whatdoestrumpthink.com/api/v1/quotes/random')
+    // getRandomQuote = () => {
+    //     fetch('https://api.whatdoestrumpthink.com/api/v1/quotes/random')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data.message);
+    //             this.setState({
+    //                 message: data.message
+    //             });
+    //         })
+    //         .catch(error => console.log(error))
+    // }
+
+    // getPersonalQuote = () => {
+    //     fetch(`https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q=${this.state.name}`)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data.message);
+    //             this.setState({
+    //                 message: data.message
+    //             });
+    //         })
+    //         .catch(error => console.log(error))
+    // }
+
+    getQuote = (url) => {
+        fetch(url)
             .then(response => response.json())
             .then(data => {
                 console.log(data.message);
@@ -28,17 +52,12 @@ class Main extends React.Component {
             .catch(error => console.log(error))
     }
 
-    getPersonalQuote = () => {
-        fetch(`https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q=${this.state.name}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.message);
-                this.setState({
-                    message: data.message
-                });
-            })
-            .catch(error => console.log(error))
-    }
+    getQuoteRandom = () => this.getQuote('https://api.whatdoestrumpthink.com/api/v1/quotes/random');
+
+    getQuotePersonal = () => this.getQuote(`https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q=${this.state.name}`);
+
+
+
 
     render(){
         return (
@@ -48,10 +67,14 @@ class Main extends React.Component {
                         <h2 className='sub-header'>*Instructions: Click the button to get a random quote from Trump, or enter your name and generate a personal quote to hear what he thinks about you.</h2>
                     </header>
                     <div className='center'>
-                        <button className='button' onClick={this.getRandomQuote}>Random Quote</button>
-                        <div className='form'>
+                        <button className='button' onClick={this.getQuoteRandom}>Random Quote</button>
+                        {/* <div className='form'>
+                            <input className='input' onChange={(el) => { this.setState({ name: el.target.value }) }} placeholder='Name' type="text" required />
                             <button className='button' onClick={this.getPersonalQuote}>Personal Quote</button>
-                            <input className='input' onChange={(el) => { this.setState({ name: el.target.value }) }} placeholder='Name' type="text" />
+                        </div> */}
+                        <div className='form'>
+                            <input className='input' onChange={(el) => { this.setState({ name: el.target.value }) }} placeholder='Name' type="text" required />
+                            <button className='button' onClick={this.getQuotePersonal}>Personal Quote</button>
                         </div>
                         <h3 className='quote'>"{this.state.message}" - Donald J. Trump</h3>
                     </div>
